db0fe7d5dddb6b92f442fa1ce049ec904003ffe9
rishu-18 <ritesh.kumar465@gmail.com>
2017-04-02 00:03:37 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<link href="vendor/css/jquery-ui.css" rel="stylesheet" type="text/css" />
+	<link href="vendor/css/jquery-ui.structure.css" rel="stylesheet" />
+	<link href="vendor/css/bootstrap.css" rel="stylesheet" />
+	<link href="style.css" rel="stylesheet" />
+
+	<script src="vendor/js/jquery.js" type="text/javascript"></script>
+	<script src="vendor/js/jquery-ui.js" type="text/javascript"></script>
+	<script src="vendor/js/bootstrap.js" type="text/javascript"></script>
+	<script src="auto_correct.js" type="text/javascript"></script>
+</head>
+<body>
+	<center><h3>AutoCorrect</h3></center>
+	<hr>
+	<center><input type="text" id="textId" class="form-control textClass" style="width:50%;" /></center>
+</body>
+</html>
